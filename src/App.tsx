6c7ff5cac03dae7c72502d01f18be760c1ff8073
unknown <mustafaalaliofficial@gmail.com>
2025-07-09 16:43:08 +0300

    Applied some font changes and updated the intro section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,17 @@ import MouseCursor from './components/MouseCursor';
 import Header from './components/Header';
 import Main from './components/Main';
 
+/**
+ * min-n = n: @media (width >= m)
+ * max-n = n: @media (width < m)
+ * 
+ *  sm: 40rem ->  640px
+ *  md: 48rem ->  768px
+ *  lg: 64rem -> 1024px
+ *  xl: 80rem -> 1280px
+ * 2xl: 96rem -> 1536px
+ */
+
 export default function App() {
   const [isMobile, setIsMobile] = React.useState(window.innerWidth < 768);
 
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,45 +4,48 @@ import HoverElement from "./HoverElement";
 export default function Main() {
   return (
     <main>
-      <section id='intro' className='px-4 py-20'>
-        <motion.div
-          className='flex items-center'
-          initial={{ x: -300, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          transition={{ type: 'tween', duration: 1 }}
-        >
-          <em className='mr-2 text-[4vw]'>I'm a</em>
-          <HoverElement className='text-[10vw]' elementType='h1' text='Full-Stack' />
-        </motion.div>
-        <motion.div
-          className='flex items-center justify-end'
-          initial={{ x: 300, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          transition={{ type: 'tween', duration: 1 }}
-        >
-          <HoverElement className='mr-[3vw] text-[10vw]' elementType='h1' text='Web' />
-          <HoverElement className='text-[10vw]' elementType='h1' text='Developer' />
-          <em className='ml-2 text-[4vw]'>& a</em>
-        </motion.div>
-        <motion.div
-          className='flex items-center'
-          initial={{ x: -300, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          transition={{ type: 'tween', duration: 1 }}
-        >
-          <HoverElement className='ml-2 text-[10vw]' elementType='h1' text='Software' />
-        </motion.div>
-        <motion.div
-          className='flex items-center justify-end'
-          initial={{ x: 300, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          transition={{ type: 'tween', duration: 1 }}
-        >
-          <HoverElement className='mr-2 text-[10vw]' elementType='h1' text='Engineer' />
-        </motion.div>
+      <section id='intro' className='min-h-screen px-4 py-20 flex items-center justify-center'>
+        <div className='lg:w-[66rem]'>
+          <motion.div
+            className='flex items-center'
+            initial={{ x: -300, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ type: 'tween', duration: 1 }}
+          >
+            <em className='mr-3 text-[4vw] lg:text-[calc(64rem*0.04)]'>I'm a</em>
+            <HoverElement className='text-[9vw] lg:text-[calc(64rem*0.09)]' elementType='h1' text='Full-Stack' />
+          </motion.div>
+          <motion.div
+            className='flex items-center justify-end'
+            initial={{ x: 300, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ type: 'tween', duration: 1 }}
+          >
+            <HoverElement className='mr-[2vw] text-[9vw] lg:mr-[calc(64rem*0.02)] lg:text-[calc(64rem*0.09)]' elementType='h1' text='Web' />
+            <HoverElement className='text-[9vw] lg:text-[calc(64rem*0.09)]' elementType='h1' text='Developer' />
+            <em className='ml-3 text-[4vw] lg:text-[calc(64rem*0.04)]'>& a</em>
+          </motion.div>
+          <motion.div
+            className='ml-5 flex'
+            initial={{ x: -300, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ type: 'tween', duration: 1 }}
+          >
+            <HoverElement className='ml-2 text-[9vw] lg:text-[calc(64rem*0.09)]' elementType='h1' text='Software' />
+          </motion.div>
+          <motion.div
+            className='mr-10 flex justify-end'
+            initial={{ x: 300, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ type: 'tween', duration: 1 }}
+          >
+            <HoverElement className='mr-2 text-[9vw] lg:text-[calc(64rem*0.09)]' elementType='h1' text='Engineer' />
+          </motion.div>
+        </div>
       </section>
       <section id='about' className='flex'>
-        <div className='relative w-[calc(100vw/2)] h-[calc(133vw/2)] max-w-[750px] max-h-[1000px] after:absolute after:inset-0 after:bg-radial after:from-[#0003] after:from-30% after:to-black after:to-75%'>
+        <p className='w-[calc(100vw/2)] px-4 py-8 text-md'>My name is Mustafa Al-Ali. I was born in 2004. I lived in Syria and went to live in Turkey when the war began.</p>
+        <div className='relative w-[calc(100vw/2)] h-[calc(133vw/2)] after:absolute after:inset-0 after:bg-radial after:from-[#0003] after:from-30% after:to-black after:to-75%'>
           <motion.div
             className='w-full h-full bg-[url(./assets/my-photo.jpg)] bg-cover bg-center'
             initial={{ opacity: 0 }}
@@ -50,7 +53,6 @@ export default function Main() {
             transition={{ type: 'tween', duration: 1 }}
           />
         </div>
-        <p className='px-6 py-8'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta consectetur reprehenderit corrupti possimus assumenda atque illum veniam, consequatur error impedit. Hic iste ducimus nam ipsum quidem sequi repellendus! Ullam, at?</p>
       </section>
       <section id='projects' className='min-h-[50vh]'>
         Projects
